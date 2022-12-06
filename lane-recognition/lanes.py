3e91aff886399891a8ae856c9faec94fabc71236
Marko Mušica <markomusica@gmail.com>
2022-12-06 16:45:16 +0100

    Added gradient recognition via "canny"

--- lane-recognition/lanes.py
+++ lane-recognition/lanes.py
@@ -8,5 +8,9 @@ if image is None:
 
 lane_image = np.copy(image)
 gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
-cv2.imshow("Display window", gray)
+
+blurred_image = cv2.GaussianBlur(gray, (5,5), 0)
+canny = cv2.Canny(blurred_image, 50, 150) # documentacion says 1:3
+cv2.imshow("Display window", canny)
 cv2.waitKey(0)
+
