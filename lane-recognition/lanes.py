b5b82f746cbf0ebd611b5ce7d1aeb9789c098750
Marko Mušica <markomusica@gmail.com>
2022-12-06 21:20:49 +0100

    Averaged the drawn lines for nicer image display

--- lane-recognition/lanes.py
+++ lane-recognition/lanes.py
@@ -3,6 +3,33 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+def make_cordinates(image, line_parameters):
+    slope, intercept = line_parameters
+    y1 = image.shape[0] #bottom left corner of image necause the max height is at the bottom of the image
+    y2 = int(y1 * (3 / 5)) # go upword 3/5 of the image
+    x1 = int((y1 - intercept) / slope) #y = mx + b therefore x = (y - b) / m
+    x2 = int((y2 - intercept) / slope)
+    return np.array([x1, y1, x2, y2])
+    
+def average_slope_intercept(image, lines):
+    left_fit = []
+    right_fit = []
+    for line in lines:
+        x1, y1, x2, y2 = line.reshape(4)
+        parameters = np.polyfit((x1, x2), (y1, y2), 1) #we get the slope and y intercept (prijevod=nagib) for a linear function y=mx + b so we get the x  and b
+        slope = parameters[0]
+        intercept = parameters[1]
+        if slope < 0: #if slope is negative then it's the left line, otherwise it's the right
+            left_fit.append((slope, intercept))
+        else:
+            right_fit.append((slope, intercept))
+            
+    left_fit_average = np.average(left_fit, axis=0)
+    right_fit_average = np.average(right_fit, axis=0)
+    left_line = make_cordinates(image, left_fit_average)
+    right_line = make_cordinates(image, right_fit_average)
+    return np.array([left_line, right_line])
+    
 def import_image(filename):
     return cv2.imread(filename)
 
@@ -14,9 +41,8 @@ def canny(image):
 def display_lines(image, lines):
     line_image = np.zeros_like(image)
     if lines is not None:
-        for line in lines:
-            x1, y1, x2, y2 = line.reshape(4)
-            cv2.line(line_image, (x1, y1), (x2, y2), (0,0,255), 10)
+        for x1, y1, x2, y2 in lines:
+            cv2.line(line_image, (x1, y1), (x2, y2), (0,255,0), 10)
     return line_image
     
 def region_of_interest(image):
@@ -29,12 +55,13 @@ def region_of_interest(image):
     masked_image = cv2.bitwise_and(image, mask)
     return masked_image
 
-image = import_image('lane-recognition/images/test_image.jpg')
+image = import_image('lane-recognition/materials/test_image.jpg')
 lane_image = np.copy(image)
 canny_image = canny(lane_image)
 cropped_image = region_of_interest(canny_image)
 lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap=5) #defining rows in pixels and radians to get a grid for Hough transformations and trying to find a bin with the most votes(lines corssing)
-line_image = display_lines(lane_image, lines)
+averaged_lines = average_slope_intercept(lane_image, lines)
+line_image = display_lines(lane_image, averaged_lines)
 
 output_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1) #makes lane_image a bit darker -- 0.8 so 20% darker to better define lines
 cv2.imshow("Image window", output_image)
