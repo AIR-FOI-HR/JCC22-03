a3d369f10ed241b537e5e0dacc5642e9b2c05e44
Marko Mušica <markomusica@gmail.com>
2023-02-09 19:07:32 +0100

    Created Recycler View and added log fetching

--- car-ai-app/app/src/main/java/com/example/caraiapp/MainActivity.kt
+++ car-ai-app/app/src/main/java/com/example/caraiapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.caraiapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.example.caraiapp.databinding.ActivityLogsFeedBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,9 +11,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-
         val binding: ActivityLogsFeedBinding = DataBindingUtil.setContentView(this, R.layout.activity_logs_feed)
-        binding.carLogText = "First Car Log Text"
+        //binding.carLogText = "First Car Log Text"
 
+        val viewModel : LogsFeedViewModel = ViewModelProvider(this)[LogsFeedViewModel::class.java]
+        binding.lifecycleOwner = this
+        binding.logsViewModel = viewModel
+
+        viewModel.fetchLogsFeed()
     }
 }
