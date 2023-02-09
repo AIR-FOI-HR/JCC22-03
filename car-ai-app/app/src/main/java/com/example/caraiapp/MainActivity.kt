432b754100ba3f639048c8296cfb64c3c31c2b28
Marko Mušica <markomusica@gmail.com>
2023-02-09 16:51:20 +0100

    Added Repository class and included data binding

--- car-ai-app/app/src/main/java/com/example/caraiapp/MainActivity.kt
+++ car-ai-app/app/src/main/java/com/example/caraiapp/MainActivity.kt
@@ -2,34 +2,17 @@ package com.example.caraiapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import com.google.firebase.database.*
-
+import androidx.databinding.DataBindingUtil
+import com.example.caraiapp.databinding.ActivityLogsFeedBinding
 
 class MainActivity : AppCompatActivity() {
-    private var firebaseDataBase: FirebaseDatabase? = null
-    private var databaseReference: DatabaseReference? = null
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        firebaseDataBase = FirebaseDatabase.getInstance()
-        databaseReference = firebaseDataBase?.getReference("users")
 
-        getData()
-    }
 
-    private fun getData() {
-        databaseReference?.addValueEventListener(object: ValueEventListener{
-            override fun onDataChange(snapshot: DataSnapshot) {
-                Log.e("Data changed", "onDataChange:$snapshot ")
-            }
+        val binding: ActivityLogsFeedBinding = DataBindingUtil.setContentView(this, R.layout.activity_logs_feed)
+        binding.carLogText = "First Car Log Text"
 
-            override fun onCancelled(error: DatabaseError) {
-                Log.e("Cancel", "onCancelled:${error.toException()} ")
-            }
-
-        })
     }
 }
---
+++ car-ai-app/app/src/main/java/com/example/caraiapp/Repository.kt
@@ -0,0 +1,24 @@
+package com.example.caraiapp
+
+import android.util.Log
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+
+class Repository {
+    /*
+    val database = Firebase.database
+    val myRef = database.getReference("logs")
+
+    myRef.addValueEventListener(object : ValueEventListener {
+        override fun onDataChange(snapshot: DataSnapshot) {
+            Log.i("Snapshot", snapshot.value.toString())
+        }
+
+        override fun onCancelled(error: DatabaseError) {
+            //Nothing
+        }
+    })*/
+}
