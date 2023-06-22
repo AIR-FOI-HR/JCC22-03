3002848303a6e2579ea765308bbf3ce1e34dcba7
kedzo13 <75586219+kedzo13@users.noreply.github.com>
2023-06-22 21:51:40 +0200

    Create logs feed

---
+++ car-ai-app/logsfeed/src/main/java/hr/foi/air/caraiapp/logsfeed/managers/DataPresenterManager.kt
@@ -0,0 +1,70 @@
+package hr.foi.air.caraiapp.logsfeed.managers
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.navigation.NavigationView
+import hr.foi.air.caraiapp.core.DataPresenter
+import hr.foi.air.caraiapp.logsfeed.R
+import hr.foi.air.caraiapp.logsfeed.ui.fragments.LogsFeedRecyclerViewFragment
+
+object DataPresenterManager {
+
+    private var dataPresenters: ArrayList<DataPresenter> = ArrayList()
+    private lateinit var activity: AppCompatActivity
+    private lateinit var navView: NavigationView
+    private lateinit var drawerLayout: DrawerLayout
+
+    fun setDependencies(
+        activity: AppCompatActivity,
+        navView: NavigationView,
+        drawerLayout: DrawerLayout,
+    ): DataPresenterManager {
+        DataPresenterManager.activity = activity
+        DataPresenterManager.navView = navView
+        DataPresenterManager.drawerLayout = drawerLayout
+
+        return this
+    }
+
+    fun initializeDataPresenters(): DataPresenterManager {
+        addDataPresenter(LogsFeedRecyclerViewFragment())
+        return this
+    }
+
+    private fun addDataPresenter(dataPresenter: DataPresenter) {
+        dataPresenters.add(dataPresenter)
+        addPresenterToMenu(dataPresenter)
+    }
+
+    private fun addPresenterToMenu(dataPresenter: DataPresenter) {
+        val context = activity.applicationContext
+        val id = dataPresenters.indexOf(dataPresenter)
+
+        navView.menu
+            .add(R.id.dynamic_group, id, id + 1, dataPresenter.getName(context))
+            .setIcon(dataPresenter.getIcon(context))
+            .setCheckable(true)
+            .setOnMenuItemClickListener {
+                showDataPresenter(dataPresenter)
+                drawerLayout.closeDrawer(GravityCompat.START)
+                true
+            }
+    }
+
+    private fun showDataPresenter(dataPresenter: DataPresenter) {
+        if (activity.supportFragmentManager.findFragmentById(dataPresenter.getFragment().id) == null) {
+            activity.supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.main_fragment, dataPresenter.getFragment())
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                .addToBackStack("")
+                .commit()
+        }
+    }
+
+    fun showMainDataPresenter() {
+        showDataPresenter(dataPresenters[0])
+    }
+}
---
+++ car-ai-app/logsfeed/src/main/java/hr/foi/air/caraiapp/logsfeed/ui/fragments/LogsFeedRecyclerViewFragment.kt
@@ -0,0 +1,90 @@
+package hr.foi.air.caraiapp.logsfeed.ui.fragments
+
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import hr.foi.air.caraiapp.core.DataPresenter
+import hr.foi.air.caraiapp.logsfeed.R
+import hr.foi.air.caraiapp.logsfeed.databinding.FragmentRecyclerViewBinding
+import hr.foi.air.caraiapp.logsfeed.ui.adapters.LogsFeedRecyclerViewAdapter
+import hr.foi.air.caraiapp.logsfeed.ui.viewmodels.LogsFeedViewModel
+
+class LogsFeedRecyclerViewFragment : Fragment(), DataPresenter {
+
+    private val viewModel: LogsFeedViewModel by viewModels()
+    private val logsFeedAdapter = LogsFeedRecyclerViewAdapter()
+
+    private lateinit var binding: FragmentRecyclerViewBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        binding = FragmentRecyclerViewBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupObservers()
+        setupUi()
+
+        viewModel.fetchCarOwners("mmusica")
+    }
+
+    private fun setupObservers() {
+        viewModel.logsFeedLiveData.observe(viewLifecycleOwner) { logItems ->
+            logsFeedAdapter.submitList(logItems)
+            binding.recyclerView.scrollToPosition(logsFeedAdapter.itemCount)
+        }
+        viewModel.carOwnersLiveData.observe(viewLifecycleOwner) { carOwners ->
+            viewModel.fetchCars(carOwners)
+        }
+        viewModel.carsLiveData.observe(viewLifecycleOwner) { cars ->
+            binding.spinner.setupAdapterAndListener(titles = cars.map { it.name })
+        }
+    }
+
+    private fun setupUi() {
+        binding.recyclerView.adapter = logsFeedAdapter
+    }
+
+    private fun Spinner.setupAdapterAndListener(titles: List<String>) {
+        adapter = ArrayAdapter(
+            context,
+            android.R.layout.simple_spinner_dropdown_item,
+            titles,
+        )
+        setSelection(selectedItemPosition)
+
+        onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) =
+                viewModel.onCarSelected(parent?.getItemAtPosition(position) as String)
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+        }
+    }
+
+    override fun getIcon(context: Context): Drawable {
+        return AppCompatResources.getDrawable(context, android.R.drawable.ic_media_play)!!
+    }
+
+    override fun getName(context: Context): String {
+        return context.getString(R.string.logs_recycler_view_presenter)
+    }
+
+    override fun getFragment(): Fragment {
+        return this
+    }
+}
