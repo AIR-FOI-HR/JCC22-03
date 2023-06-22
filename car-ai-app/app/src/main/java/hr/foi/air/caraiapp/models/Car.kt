091670c219d5787250c7aea77ddd21fd75809b88
kedzo13 <75586219+kedzo13@users.noreply.github.com>
2023-06-22 21:51:40 +0200

    Create logs feed

--- car-ai-app/app/src/main/java/hr/foi/air/caraiapp/models/Car.kt
+++
@@ -1,6 +0,0 @@
-package hr.foi.air.caraiapp.models
-
-data class Car(
-    val id: String,
-    val name: String,
-)
---
+++ car-ai-app/logsfeed/src/main/java/hr/foi/air/caraiapp/logsfeed/ui/LogsFeedActivity.kt
@@ -0,0 +1,69 @@
+package hr.foi.air.caraiapp.logsfeed.ui
+
+import hr.foi.air.caraiapp.core.R as coreR
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import com.google.android.material.navigation.NavigationView
+import hr.foi.air.caraiapp.logsfeed.R
+import hr.foi.air.caraiapp.logsfeed.databinding.ActivityLogsFeedBinding
+import hr.foi.air.caraiapp.logsfeed.managers.DataPresenterManager
+
+class LogsFeedActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var binding: ActivityLogsFeedBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityLogsFeedBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        initializeLayout()
+        initializeDataPresenterManager()
+    }
+
+    private fun initializeDataPresenterManager() {
+        DataPresenterManager
+            .setDependencies(this, binding.navView, binding.drawerLayout)
+            .initializeDataPresenters()
+            .showMainDataPresenter()
+    }
+
+    private fun initializeLayout() {
+        binding.layoutMain.toolbar.setTitleTextColor(getColor(coreR.color.white))
+        setSupportActionBar(binding.layoutMain.toolbar)
+        val drawerToggle = ActionBarDrawerToggle(
+            this,
+            binding.drawerLayout,
+            binding.layoutMain.toolbar,
+            R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        )
+        binding.drawerLayout.addDrawerListener(drawerToggle)
+        drawerToggle.syncState()
+
+        binding.navView.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onBackPressed() {
+        if (binding.drawerLayout.isDrawerOpen(GravityCompat.START))
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+        else
+            super.onBackPressed()
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.menu_about) {
+            Toast.makeText(
+                this,
+                "JCC - Marko Mušica, Karlo Gardijan, Josipa Meštrović",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+        return true
+    }
+}
